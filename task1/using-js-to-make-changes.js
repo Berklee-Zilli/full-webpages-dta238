adf5d1d05707def73276683b8988c3930ad3e3b6
gax <gax@clifford-the-big-red-macbook.local>
2024-11-19 19:51:59 -0500

    uaguygauiayfd

---
+++ task1/using-js-to-make-changes.js
@@ -0,0 +1,8 @@
+//1
+document.title = "Title by Devon Thomas Ambrosi"
+//2
+document.querySelector('header h1').textContent = "Task 1"
+//3
+document.querySelector('header p').textContent = "I'm a 6'2 college student in my junior year of Berklee College of Music"
+//4
+document.querySelector('section h2').textContent = "List of things I like"
