89305159f0367cddd72bbf9bae9c4e104262301a
gax <gax@clifford-the-big-red-macbook.local>
2024-11-19 19:53:55 -0500

    Level 1 Complete

--- task1/using-js-to-make-changes.js
+++ task1/using-js-to-make-changes.js
@@ -6,3 +6,4 @@ document.querySelector('header h1').textContent = "Task 1"
 document.querySelector('header p').textContent = "I'm a 6'2 college student in my junior year of Berklee College of Music"
 //4
 document.querySelector('section h2').textContent = "List of things I like"
+
